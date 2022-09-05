fc345dd6097f88d703ba6296e87fb353502aca59
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2022-09-06 00:27:21 +0500

    Minor Changes Made

--- pages/api/class/deleteclass.js
+++ pages/api/class/deleteclass.js
@@ -4,6 +4,7 @@ import connectDb from '../../../middleware/mongoose'
 
 const handler = async (req, res) => {
     if(req.method == 'DELETE'){
+
         /*
             { "_id" : ObjectId("5e8ca85cef4dcbee04fbbc12"), "software" : { "services" : [ "whatsapp", "twitter" ] } }
 
@@ -13,37 +14,19 @@ const handler = async (req, res) => {
         try{
             // Teacher is Deleting Whole Class
             if(req.body.isTeacher){
-                res.send({success: "ADDD DDDsClass Deleted Successfully"})
+                res.send({success: "Admin has deleted the class successfully"})
                 await Users.findByIdAndRemove({_id: req.body.userID} ) // Deletes Entire User
             }
+            
             // Class Member is Leaving Class
             else{
-                let sass = await Users.findByIdAndUpdate({_id: req.body.userID}, {$pull: { classesJoined: req.body.classID } })
-                // await Users.findByIdAndUpdate({_id: req.body.userID}, { $push: { classesJoined: req.body.classID } })
+                // await Users.findByIdAndUpdate({_id: req.body.userID}, { $pull: { classesJoined: req.body.classID } })
 
                 // await Classroom.findByIdAndUpdate({_id: req.body.classID}, { $pull: { classroomMembers: req.body.userID } })
-                // await Classroom.findByIdAndUpdate({_id: req.body.classID}, { $push: { classroomMembers: req.body.userID } })
-                
-                
-                // const classExist = await Users.find( { _id: req.body.userID}, {classesJoined: req.body.classID } )
 
-                // const classExist = await Classroom.find({_id: req.body.classID}, { classroomMembers: req.body.userID })
+                // const class = await Classroom.find({_id: req.body.classID}, { classroomMembers: req.body.userID })
 
-                res.send(sass)
-
-                // findAndModify({
-                //     query: { category: "cafe", status: "a" },
-                //     sort: { category: 1 },
-                //     update: { $set: { status: "Updated" } },
-                //     collation: { locale: "fr", strength: 1 }
-                // });
-
-                
-                // const userClass = await Users.find( { _id: req.body.userID}, {classesJoined: req.body.classID } )
-                // res.send(userClass[0].classesJoined)
-
-                // res.send({success: "Class Removed Successfully"})
-                // res.send(classExist[0].classesJoined)
+                // const user = await Users.find( { _id: req.body.userID}, {classesJoined: req.body.classID } )
             }
 
         } catch (error) {
