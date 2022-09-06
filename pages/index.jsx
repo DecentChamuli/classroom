d816aea491b2dfaeec2da6dda539211e5b842e4b
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2022-09-07 00:03:36 +0500

    No Logged In user redirected to login page automatically

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.scss'
 import AuthContext from '../stores/authContext'
 import Cookies from 'js-cookie'
@@ -10,11 +11,13 @@ import { DotsLoader } from '../components/Loader'
 
 export default function Home() {
 
+  const router = useRouter()
+
   const authContext = useContext(AuthContext)
 
   const [userClasses, setClasses] = useState([])
   const [isLoading, setLoading] = useState(false)
-  
+
   let UserID = authContext.userID
   let setUserID = authContext.setUserID
 
@@ -31,58 +34,48 @@ export default function Home() {
         setLoading(false)
         return 
       }
+      else{
+        router.push('/login')
+      }
     }
     fetchClass()
-  }, [UserID, setUserID])
+  }, [UserID, router, setUserID])
 
   return (
-  
     <div>
       <Head>
         <title>Classroom</title>
         <meta name="description" content="Classroom built by Muhammad Tahir Ali" />
       </Head>
       {isLoading ? <DotsLoader /> : <>
-        {!UserID ?
+        {!userClasses.length ?
           <div className={styles.nothing}>
             <div className={styles.container}>
-              <p className={styles.loginText}>Login to View Classes</p>
-              <Link href="/login"><button className={styles.btn}>Join Now</button></Link>
+              <p>Click <AiOutlinePlus className={styles.icon} /> to Create or Join Class </p>
             </div>
           </div>
           :
-          <>
-            {!userClasses.length ?
-              <div className={styles.nothing}>
-                <div className={styles.container}>
-                  <p>Click <AiOutlinePlus className={styles.icon} /> to Create or Join Class </p>
-                </div>
+          <main className={styles.main}>
+            <div className={styles.container}>
+              <div className={styles.allClasses}>
+                {userClasses.map((userClass, index) => (
+                  <Link href={`/class/${userClass.classroomSlug}`} key={index}>
+                    <div className={styles.eachClass}>
+                      <div className={styles.top}>
+                        <h3>{userClass.classroomName}</h3>
+                      </div>
+                      <div className={styles.bottom}>
+                        <span>{userClass.classroomCode}</span>
+                      </div>
+                    </div>
+                  </Link>
+                ))}
               </div>
-              :
-              <main className={styles.main}>
-                <div className={styles.container}>
-                  <div className={styles.allClasses}>
-                    {userClasses.map((userClass, index) => (
-                      <Link href={`/class/${userClass.classroomSlug}`} key={index}>
-                        <div className={styles.eachClass}>
-                          <div className={styles.top}>
-                            <h3>{userClass.classroomName}</h3>
-                          </div>
-                          <div className={styles.bottom}>
-                            <span>{userClass.classroomCode}</span>
-                          </div>
-                        </div>
-                      </Link>
-                    ))}
-                  </div>
-                </div>
-                <h2 style={{padding: '10px 40px 0px 40px', color: '#e42e27'}}>This Website is currently under CONSTRUCTION, so USER can experience some crashes and unstable UI</h2>
-              </main>
-            }
-          </>
+            </div>
+            <h2 style={{ padding: '10px 40px 0px 40px', color: '#e42e27' }}>This Website is currently in DEVELOPMENT MODE, so USER can experience some crashes and unstable UI</h2>
+          </main>
         }
-        </>
-      }
+      </>}
     </div>
   )
 }
