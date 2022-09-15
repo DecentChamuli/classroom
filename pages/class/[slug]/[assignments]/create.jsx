b282c749305f1afe8de308a15502f9d7b63d45b7
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2022-09-15 16:17:40 +0500

    Create Assignment UI Done 	modified:   pages/class/[slug]/[assignments]/create.jsx 	modified:   styles/CreateAssignment.module.scss

--- pages/class/[slug]/[assignments]/create.jsx
+++ pages/class/[slug]/[assignments]/create.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import styles from '../../../../styles/CreateAssignment.module.scss'
 import AuthContext from '../../../../stores/authContext'
-import { FiUpload } from 'react-icons/fi'
+import { FiUpload, FiChevronDown, FiChevronUp } from 'react-icons/fi'
 
 const Create = () => {
 
@@ -12,6 +12,7 @@ const Create = () => {
   const { slug } = router.query
 
   const [value, setValue] = useState("")
+  const [showFile, setShowfile] = useState(false)
 
   // const authContext = useContext(AuthContext)
   // let UserID = authContext.userID
@@ -38,26 +39,29 @@ const Create = () => {
         <meta name="description" content="Classroom built by Muhammad Tahir Ali" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.container}>
-          <div className={styles.inputField}>
-            <input id='classDesc' placeholder=" " className={styles.inputBox} type="text" />
-            <label htmlFor="classDesc" className={styles.inputLabel}>Enter File URL</label>
-          </div>
-          <div className={styles.uploadFile}>
-            <input type="file" id="assignmentFile" onChange={(e) => handleUpload(e)} />
-            <label htmlFor="assignmentFile">
-              <div className={styles.btn}><span><FiUpload /></span>Upload File</div>
-            </label>
-          </div>
-          <div className={`${styles.btn} ${styles.btn2}`}>Submit</div>
-          <div className={styles.marks}>
-            <label>
-              Total Marks
-              <input type="number" value={value} onChange={(e) => setValue(e.target.value)} />
-            </label>
-          </div>
-          <button className={styles.btn} disabled={value ? false : true} onClick={() => handleSubmit()}>Create Assignment</button>
+        <h1 className={styles.heading}>Create Assignment</h1>
+        <div className={styles.inputField}>
+          <input id='assignmentTitle' placeholder=" " className={styles.inputBox} type="text" />
+          <label htmlFor="assignmentTitle" className={styles.inputLabel}>Assignment Title</label>
         </div>
+        <div className={styles.inputField} style={{height: '150px'}}>
+          <textarea id='assignmentDesc' style={{fontSize: '18px', padding: '10px 1rem', fontWeight: '300'}} placeholder=" " className={styles.inputBox} />
+          <label htmlFor="assignmentDesc" className={styles.inputLabel}>Assignment Description</label>
+        </div>
+        <div className={styles.uploadFile}>
+          <p onClick={()=> setShowfile(!showFile)}>Upload Reference File <span style = {showFile ? {rotate: '-180deg'} : {}}><FiChevronDown /></span></p>
+          <input type="file" id="assignmentFile" onChange={(e) => handleUpload(e)} />
+          {!showFile && <><label htmlFor="assignmentFile">
+            <div className={styles.btn}><span><FiUpload /></span>Upload File</div>
+          </label></>}
+        </div>
+        <div className={styles.marks}>
+          <label>
+            Total Marks
+            <input type="number" value={value} onChange={(e) => setValue(e.target.value)} />
+          </label>
+        </div>
+        <div className={`${styles.btn} ${styles.btn2}`} disabled={value ? false : true} onClick={() => handleSubmit()}>Create Assignment</div>
       </main>
     </div>
   )
