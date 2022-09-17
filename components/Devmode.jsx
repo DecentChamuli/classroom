c1a169851ffe176198c8a5d9c6e2ad9c0c2a3c47
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2022-09-17 12:33:35 +0500

    Dev Mode Warning Render by Component

---
+++ components/Devmode.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Devmode = () => {
+  return (
+    <h2 style={{ padding: '10px 40px 0px 40px', color: '#e42e27', textAlign: 'center' }}>This Website is currently in DEVELOPMENT MODE, so USER can experience some crashes and unstable UI</h2>
+  )
+}
+
+export default Devmode
