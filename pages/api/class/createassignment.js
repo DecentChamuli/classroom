1a2b78cb64ccf41a85ff610c5f63993e09294e42
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2022-09-21 22:13:14 +0500

    Assignment Slug is ID

--- pages/api/class/createassignment.js
+++ pages/api/class/createassignment.js
@@ -1,5 +1,6 @@
 import Classroom from '../../../models/Classroom'
 import connectDb from '../../../middleware/mongoose'
+import { nanoid } from 'nanoid'
 
 const handler = async (req, res) => {
     if(req.method == 'POST'){
@@ -7,13 +8,14 @@ const handler = async (req, res) => {
             taskTitle: req.body.taskTitle,
             taskDesc: req.body.taskDesc,
             taskMarks: req.body.taskMarks,
+            taskSlug: nanoid(15),
             dueDate: req.body.dueDate
         }
         let classroomSlug = {classroomSlug: req.body.classroomSlug}
 
         try{
             const createAsignment = await Classroom.findOneAndUpdate(classroomSlug, { $push: { classroomAssignment: assignmentData } }, { new: true })
-            res.send({success: createAsignment.classroomAssignment[createAsignment.classroomAssignment.length - 1]._id})
+            res.send({success: createAsignment.classroomAssignment[createAsignment.classroomAssignment.length - 1].taskSlug})
         } catch (error) {
             res.send({error: "Something went Wrong! Please try Again Later."})
         }
