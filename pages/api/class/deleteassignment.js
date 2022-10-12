1e05074f65fc04cd2b2c88542aa1629b8836f1a7
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2022-10-13 00:39:35 +0500

    Assignment Deletion and Nested Fields updation done

---
+++ pages/api/class/deleteassignment.js
@@ -0,0 +1,59 @@
+import Classroom from '../../../models/Classroom'
+import Users from '../../../models/Users'
+import connectDb from '../../../middleware/mongoose'
+
+const handler = async (req, res) => {
+    if(req.method == 'POST'){
+        try{
+            const classInfo = await Classroom.findOne({ classroomSlug: req.body.classroomSlug })
+            // res.send(classInfo)
+
+            /*
+            Donot Delete This Whole Line... Separate Array
+            // const user = await Users.aggregate([{$unwind: "$classesJoined"}])
+            // const user = await Users.aggregate([
+            //     { $unwind: "$classesJoined" },
+            //     { $unwind: "$classesJoined.assignment" },
+            //     { $match : { 'classesJoined.assignment.assignmentID' : req.body.taskSlug } },
+            //     { $unset: 'classesJoined.assignment' }
+            // ])
+            */
+
+            
+            const user = await Users.updateOne(
+                { _id: req.body.userID },
+                { $pull: {"classesJoined.$[classesJoined].assignment": { assignmentID: req.body.taskSlug } } },
+                { arrayFilters: [ {"classesJoined.classID" : classInfo._id} ] }
+            )
+
+
+            /*
+            Donot Delete Below Code.... Update Nested Document Values
+            const user = await Users.updateOne({ _id: req.body.userID }, 
+                { $set: {
+                    "classesJoined.$[updateClassesJoined].assignment.$[updateAssignment].obtainedMarks" : 0,
+                    "classesJoined.$[updateClassesJoined].assignment.$[updateAssignment].assignmentMarked" : false
+                }}, 
+                {
+                    "arrayFilters": [
+                    {"updateClassesJoined.classID" : classInfo._id},
+                    {"updateAssignment.assignmentID" : req.body.taskSlug}
+                ]
+            })
+            */
+
+            res.send({success: 'Asignment has been Unsubmitted', user})
+            
+            // res.send(user)
+
+        } catch (error) {
+            res.send({error: error.message})
+            // res.send({error: "Something went Wrong! Please try Again Later."})
+        }
+    }
+    else{
+        res.status(404).json({error: "Page Not Found"})
+    }
+}
+
+export default connectDb(handler)
