f6fced48c2ba14c336f6cb1dde5a23f6169d3a64
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2022-10-20 18:52:11 +0500

    . modified:   package-lock.json modified:   package.json

--- pages/class/[slug]/[assignments]/create.jsx
+++ pages/class/[slug]/[assignments]/create.jsx
@@ -8,11 +8,13 @@ import { CircularLoader } from '../../../../components/Loader'
 import axios from 'axios'
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
+import { PickerInline } from 'filestack-react'
 
 const Create = () => {
 
   const router = useRouter()
   const { slug } = router.query
+
 
   const [marks, setMarks] = useState("10")
   const [date, setDate] = useState(null)
@@ -51,21 +53,42 @@ const Create = () => {
   const handleUpload = async (event) => {
     // const fileName = event.target.files[0].name
     // const file = URL.createObjectURL(event.target.files[0])
-    const file = event.target.files[0]
+    // const file = event.target.files[0]
 
-    let form = new FormData();
-    form.append('image', file)
-
+    let form = new FormData()
     let IMGBB_API_KEY = process.env.NEXT_PUBLIC_IMGBB_API_KEY
-    
-    try {
-      const res = await axios.post(`https://api.imgbb.com/1/upload?key=${IMGBB_API_KEY}`, form)
-      console.log(res.data.data.url)
-    } catch (error) {
-      console.log(error)
-    }
+
+    form.append('image', event.target.files[0])
+    let xhr = new XMLHttpRequest()
+    xhr.upload.addEventListener("progress", ProgressHandler, false)
+    xhr.addEventListener("load", SuccessHandler, false)
+    xhr.open("POST", `https://api.imgbb.com/1/upload?key=${IMGBB_API_KEY}`)
+    xhr.send(form)
+
+    // for(const eachFile of event.target.files){
+    //   form.append('image', eachFile)
+    //   try {
+    //     console.log(eachFile)
+    //     const res = await axios.post(`https://api.imgbb.com/1/upload?key=${IMGBB_API_KEY}`, form)
+    //     console.log(res.data.data.url)
+    //   } catch (error) {
+    //     console.log(error.response.data)
+    //   }
+    // }
+
     event.target.value = null
   } 
+  const ProgressHandler = (e) => {
+    let percent = (e.loaded / e.total) * 100
+    percent = Math.round(percent)
+    if(percent % 10 === 0){
+      console.log(`Loaded: ${(e.loaded/1024**2).toFixed(2)} MBs  Total: ${(e.total/1024**2).toFixed(2)} MBs  Percentage ${percent}%`)
+    }
+  }
+  const SuccessHandler = (e) => {
+    const res = JSON.parse(e.target.response)
+    console.log(res.data.url)
+  }
 
   return (
     <div>
@@ -88,10 +111,21 @@ const Create = () => {
           </div>
           <div className={styles.uploadFile}>
             <p onClick={()=> setShowfile(!showFile)}>Upload Reference File <span style = {showFile ? {rotate: '-180deg'} : {}}><FiChevronDown /></span></p>
-            <input type="file" id="assignmentFile" onChange={(e) => handleUpload(e)} />
+            <input type="file" accept="image/png, image/jpg, image/jpeg" multiple id="assignmentFile" onChange={(e) => handleUpload(e)} />
             {showFile && <><label htmlFor="assignmentFile">
-              <small>Reference File is Optional</small>
+              <div style={{display: 'flex', flexDirection: 'column'}}>
+                <small>Reference File is Optional</small>
+                <small style={{fontSize: '70%'}}>Allowed File Type: PNG, JPEG, JPG.</small>
+              </div>
               <div className={styles.btn}><span><FiUpload /></span>Upload File</div>
+              <PickerInline 
+                apikey='AQ3Pi5Rf9RMGHvcJmU6CHz'
+                clientOptions={{
+                  accept: 'image/*'
+                  // accept: [ 'image/*', 'text/*' ]
+                }}
+                onUploadDone={(res) => console.log(res)}
+              />
             </label></>}
           </div>
         </div>
